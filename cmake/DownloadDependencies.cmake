898bfd75fc4553546c6d28d8cb392789707627ba
Blackflame576 <88478228+Blackflame576@users.noreply.github.com>
2024-09-01 14:48:17 +0300

    Update DownloadDependencies.cmake

--- cmake/DownloadDependencies.cmake
+++ cmake/DownloadDependencies.cmake
@@ -16,13 +16,13 @@ FetchContent_Declare(Client
 FetchContent_MakeAvailable(Client)
 
 # fmt
-FetchContent_Declare(fmt
-  GIT_REPOSITORY https://github.com/DeepForge-Technology/fmt.git
-  GIT_TAG master
-)
+#FetchContent_Declare(fmt
+#  GIT_REPOSITORY https://github.com/DeepForge-Technology/fmt.git
+#  GIT_TAG master
+#)
 # if(NOT fmt_POPULATED)
 #   FetchContent_Populate(fmt)
 
 #   add_subdirectory(${fmt_SOURCE_DIR} ${fmt_BINARY_DIR} EXCLUDE_FROM_ALL)
 # endif()
-FetchContent_MakeAvailable(fmt)
+#FetchContent_MakeAvailable(fmt)
