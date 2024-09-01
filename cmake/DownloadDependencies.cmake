5af060b9bf015772acc8c03d7325775115ce09ff
Blackflame576 <88478228+Blackflame576@users.noreply.github.com>
2024-09-01 14:18:01 +0300

    Update DownloadDependencies.cmake

--- cmake/DownloadDependencies.cmake
+++ cmake/DownloadDependencies.cmake
@@ -1,14 +1,14 @@
 # jsoncpp
-FetchContent_Declare(jsoncpp
-        GIT_REPOSITORY https://github.com/DeepForge-Technology/jsoncpp.git
-        GIT_TAG master
-)
+#FetchContent_Declare(jsoncpp
+#        GIT_REPOSITORY https://github.com/DeepForge-Technology/jsoncpp.git
+#        GIT_TAG master
+#)
 # if(NOT jsoncpp_POPULATED)
 #   FetchContent_Populate(jsoncpp)
 #   add_subdirectory(${jsoncpp_SOURCE_DIR} ${jsoncpp_BINARY_DIR})
 # endif()
-FetchContent_MakeAvailable(jsoncpp)
-
+#FetchContent_MakeAvailable(jsoncpp)
+find_package(jsoncpp)
 FetchContent_Declare(Client
         GIT_REPOSITORY https://github.com/DeepForge-Tech/LogClient.git
         GIT_TAG master
